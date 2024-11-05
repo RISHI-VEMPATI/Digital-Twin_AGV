1331a2b04a25ec3dfc0677a91752a0248301c779
rishi.vempati <vempatirishi1708@gmail.com>
2024-11-06 00:44:21 +0530

    third commit

--- DigitalTwinAGV/MainPC/project_platform/dashboard/models.py
+++ DigitalTwinAGV/MainPC/project_platform/dashboard/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class JobHistory(models.Model):
+    job_name = models.CharField(max_length=100)
+    arrival = models.DateTimeField()
+    due = models.DateTimeField()
+    picked_at = models.CharField(max_length=10)
+    placed_at = models.CharField(max_length=10)
+    estimated_time = models.DurationField()
+    actual_time = models.DurationField()
+    delay = models.DurationField(null=True, blank=True)
+    on_time = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.job_name
--- DigitalTwinAGV/MainPC/project_platform/dashboard/views.py
+++ DigitalTwinAGV/MainPC/project_platform/dashboard/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import JobHistory
 
 # Create your views here.
 def dashboard(request):
     return render(request, 'dashboard/dashboard.html')
 
 def history(request):
-    return render(request, 'dashboard/history.html')
+    jobs = JobHistory.objects.all()
+    return render(request, 'dashboard/history.html', {'jobs': jobs})
 
 def future(request):
     return render(request, 'dashboard/future.html')
