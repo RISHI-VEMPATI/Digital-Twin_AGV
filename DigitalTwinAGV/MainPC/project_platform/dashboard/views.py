527410ac20814e7cdee2c1e7a9cc7959bf9a6031
rsk9607 <108059528+rsk9607@users.noreply.github.com>
2024-11-06 00:43:40 +0530

    new csss

--- DigitalTwinAGV/MainPC/project_platform/dashboard/views.py
+++ DigitalTwinAGV/MainPC/project_platform/dashboard/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import random
 
 # Create your views here.
 def dashboard(request):
-    return render(request, 'dashboard/dashboard.html')
+    agv_position = random.randint(0, 100)
+    return render(request, 'dashboard/dashboard.html', {'agv_position': agv_position})
 
 def history(request):
     return render(request, 'dashboard/history.html')
